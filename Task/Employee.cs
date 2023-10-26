9b57fcc789a875207794824da1373d9aa84e2797
AleksandrA010 <dark_letsplay@mail.ru>
2023-10-26 23:38:17 +0300

    Add project files.

---
+++ Task/Employee.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task
+{
+    class Employee
+    {
+        private Dictionary<Departments, int> departments = new Dictionary<Departments, int>()
+        {
+            { Departments.ChiefDirector, 1},
+            { Departments.FinanceDirector, 2},
+            { Departments.Accounting, 3 },
+            { Departments.DirectorOfAutomation, 2 },
+            { Departments.InformationTechnologyDepartment, 3 },
+            { Departments.SystemsAnalyst, 4 },
+            { Departments.SystemsAnalystEmployee, 5 },
+            { Departments.Development, 4 },
+            { Departments.DevelopmentEmployee, 5 },
+        };
+        private string name;
+        private Departments department;
+        private int access;
+        private int ID;
+        private static int id = 0;
+        private int key = 02050;
+        private List<string> CurrentTasks = new List<string>();
+        public Employee(string name, Departments department, int ID)
+        {
+            this.name = name;
+            this.department = department;
+            this.ID = ID;
+            access = departments[department];
+        }
+        static public bool TryToDepartment(string department, out Departments DeDepartment)
+        {
+            bool flag = false;
+            switch (department)
+            {
+
+                case "Development": 
+                    DeDepartment = Departments.Development;
+                    flag = true;
+                    break;
+                case "DevelopmentEmployee":
+                    DeDepartment = Departments.DevelopmentEmployee;
+                    flag = true;
+                    break;
+                case "InformationTechnologyDepartment":
+                    DeDepartment = Departments.InformationTechnologyDepartment;
+                    flag = true;
+                    break;
+                case "FinanceDirector":
+                    DeDepartment = Departments.FinanceDirector;
+                    flag = true;
+                    break;
+                case "SystemsAnalystEmployee":
+                    DeDepartment = Departments.SystemsAnalystEmployee;
+                    flag = true;
+                    break;
+                case "Accounting":
+                    DeDepartment = Departments.Accounting;
+                    flag = true;
+                    break;
+                case "DirectorOfAutomation":
+                    DeDepartment = Departments.DirectorOfAutomation;
+                    flag = true;
+                    break;
+                case "SystemsAnalyst":
+                    DeDepartment = Departments.SystemsAnalyst;
+                    flag = true;
+                    break;
+                default:
+                    DeDepartment = Departments.Development;
+                    Console.WriteLine("Такого отдела нет.");
+                    break;
+            }
+            return flag;
+        }
+        public int GetID()
+        {
+            return ID;
+        }
+        public static void PrintDepartments()
+        {
+            foreach (var item in Enum.GetValues(typeof(Departments)))
+                Console.WriteLine(item);
+        }
+        static public bool TryToDepartment(string department)
+        {
+            bool flag = false;
+            switch (department)
+            {
+
+                case "Development":
+                    flag = true;
+                    break;
+                case "DevelopmentEmployee":
+                    flag = true;
+                    break;
+                case "InformationTechnologyDepartment":
+                    flag = true;
+                    break;
+                case "FinanceDirector":
+                    flag = true;
+                    break;
+                case "SystemsAnalystEmployee":
+                    flag = true;
+                    break;
+                case "Accounting":
+                    flag = true;
+                    break;
+                case "DirectorOfAutomation":
+                    flag = true;
+                    break;
+                case "SystemsAnalyst":
+                    flag = true;
+                    break;
+                default:
+                    Console.WriteLine("Такого отдела нет.");
+                    break;
+            }
+            return flag;
+        }
+        private void PrintTasks(List<string> CurrentTasks)
+        {
+            foreach (string item in CurrentTasks)
+            {
+                Console.WriteLine($"|{item}");
+            }
+        }
+        public void PrintEmployee()
+        {
+            Console.WriteLine($"Имя — {name}");
+            Console.WriteLine($"Отдел — {department}");
+            Console.WriteLine($"ID — #{key}{ID}");
+            Console.WriteLine($"Точка доступа — {access}");
+            Console.WriteLine("Текущие задачи:\n");
+            PrintTasks(CurrentTasks);
+        }
+        public void AddTask(string Task) 
+        {
+            CurrentTasks.Add(Task);
+        }
+        public static int NewID()
+        {
+            id++;
+            return id;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task
+{
+    enum Departments
+    {
+        ChiefDirector,
+        FinanceDirector,
+        Accounting,
+        DirectorOfAutomation,
+        InformationTechnologyDepartment,
+        SystemsAnalyst,
+        SystemsAnalystEmployee,
+        Development,
+        DevelopmentEmployee
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            bool flag = true;
+            string name;
+            string department;
+            Employee employeeFrom;
+            Employee employeeTo;
+            var Employees = new List<Employee>();
+            Employees.Add(new Employee("Tom", Departments.Development, Employee.NewID()));
+            Employees.Add(new Employee("Beer", Departments.SystemsAnalyst, Employee.NewID()));
+            Employees.Add(new Employee("Cool", Departments.SystemsAnalystEmployee, Employee.NewID()));
+            while (flag)
+            {
+                Console.WriteLine("-------------------------------------------------------------\n");
+                Console.WriteLine("break — выход из программы.");
+                Console.WriteLine("getEmployees — получить информацию о всех работниках выбранного отдела.");
+                Console.WriteLine("getDepartments — получить информацию о всех отделах");
+                Console.WriteLine("setTask — присвоить задачу сотруднику.");
+                Console.WriteLine("setEmployee — создать нового сотрудника в выбранный отдел.");
+                Console.WriteLine("deleteEmployee — удалить сотрудника в выбранном отделе.");
+                Console.WriteLine("\n-------------------------------------------------------------\n");
+                Console.Write("Введите команду: ");
+                string answer = Console.ReadLine();
+                Console.Clear();
+                switch (answer)
+                {
+                    case "break":
+                        flag = false;
+                        break;
+                    case "getEmployees":
+                        for (int i = 0; i < Employees.Count; i++)
+                        {
+                            Employees[i].PrintEmployee();
+                        }
+                        Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "getDepartments":
+                        Employee.PrintDepartments();
+                        Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "setTask":
+                        Console.Write("Введите ID сотрудника, который даёт задание: ");
+                        int IDFrom = int.Parse(Console.ReadLine());
+                        Console.Write("Введите ID сотрудника, который получает задание: ");
+                        int IDTo = int.Parse(Console.ReadLine());
+                        foreach (var item in Employees)
+                        {
+                            if (item.GetID() == IDFrom)
+                            {
+                                employeeFrom = item;
+                            }
+                            else if (item.GetID() == IDTo)
+                            {
+                                employeeTo = item;
+                            }
+                        }
+                        Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "setEmployee":
+                        Console.Write("Введите имя нового сотрудника: ");
+                        name = Console.ReadLine();
+                        Employee.PrintDepartments();
+                        Console.Write("Введите отдел в который хотите определить сотрудника: ");
+                        department = Console.ReadLine();
+                        Employee.TryToDepartment(department, out Departments DeDepartment);
+                        Employees.Add(new Employee(name, DeDepartment, Employee.NewID()));
+                        Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "deleteEmployee":
+                        Console.Write("Введите ID сотрудника, которого хотите удалить: ");
+                        int IDDelete = int.Parse(Console.ReadLine());
+                        foreach (var item in Employees)
+                        {
+                            if (item.GetID() == IDDelete)
+                            {
+                                Employees.Remove(item);
+                            }
+                        }
+                            Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Команда введена не правильно.");
+                        Console.Write("Для продолжение нажмите любую клавишу: ");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
